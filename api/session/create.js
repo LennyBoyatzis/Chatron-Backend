146e55ee434ee273524d6bdc1486ebcddf1bb68a
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-05 16:23:34 +1100

    JWT creation on validation of users creds

--- api/session/create.js
+++ api/session/create.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const jwt = require('jwt-simple')
+const moment = require('moment')
+const mongoose = require('mongoose')
+const async = require('async')
+const UserSchema = require('../models/User')
+const User = mongoose.model('User', UserSchema)
 
 // In a real world app, this would be set in .env file
 const JWT_TOKEN_SECRET = 'chatron-chat-app'
+const tokenExpiry = moment().add(7, 'days').valueOf();
 
 module.exports = (req, res) => {
-  res.status(200).json({ user: "leny" })
+  User
+    .findOne({ username: req.body.username })
+    .exec((err, user) => {
+      if (err) return res.status(500).json(err)
+      if (!user) return res.status(404).json({ err: 'Invalid user' })
+
+      if ( user.password === req.body.password ) {
+        const token = jwt.encode({
+          iss: req.body.username,
+          exp: tokenExpiry
+        }, JWT_TOKEN_SECRET)
+
+        return res.json({
+          user: user,
+          token: token,
+          expires: tokenExpiry
+        })
+      }
+    })
 }
