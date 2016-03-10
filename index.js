ceb431ad6bae87a3a8233474fc385d91855fc2bd
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-11 09:34:55 +1100

    Closing a session

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function start () {
   app.use(bodyParser.json())
   app.post('/api/signup', require('./api/signup'));
   app.post('/api/sessions/create', require('./api/session/create'));
+  app.post('/api/sessions/close', require('./api/session/close'));
   app.get('/api/user/get', require('./api/user/get'));
   app.use((req, res) => res.status(404).json({ error: '404 Not Found' }))
 
